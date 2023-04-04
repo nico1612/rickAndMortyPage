b664b307f8a3c80478d1301675969afca02f6dbe
nico1612 <nicogonzalezraggi123@hotmail.com>
2023-04-04 13:17:14 -0300

    personaje

--- src/personajes/pages/personajePage.jsx
+++ src/personajes/pages/personajePage.jsx
@@ -1,31 +1,24 @@
 import { useEffect, useState } from "react"
-import { useDispatch, useSelector } from "react-redux"
 import { useParams } from "react-router-dom"
-import { getPersonajes } from "../../slice/personajes"
-
 
 export const PersonajePage=()=>{
 
     const {id}= useParams()
-    const [personaje,setPersonaje] =useState({})
     const ids=parseInt(id,10)
 
-    const {personajes=[],page} =useSelector(state=>state.personajes)
+    const [personaje,setPersonaje] =useState({})    
 
-    const dispatch=useDispatch()
-
-    useEffect(async()=>{
-        const resp = await fetch(`https://rickandmortyapi.com/api/character/${id}`);
-        const data = await resp.json();
-        console.log(data)
-        setPersonaje(data.results)
+    useEffect(()=>{
+        const cargarPersonaje=async()=>{
+            const resp = await fetch(`https://rickandmortyapi.com/api/character/${ids}`);
+            const data = await resp.json();
+            setPersonaje(data)
+            console.log(personaje)
+        }
+        cargarPersonaje()
     },[])
 
-    //console.log(personaje)
-    setPersonaje()
-        
-    setPersonaje(Object.values(personaje))
-    
+     
     return (
 
         <>
@@ -33,20 +26,20 @@ export const PersonajePage=()=>{
             <div className="row mt-5">
                 <div className="col-4">
                     <img
-                        src={personajes[ids-1].image}
-                        alt={personajes[ids-1].name}
+                        src={personaje.image}
+                        alt={personaje.name}
                         className="img-thumbnail animate__animated animate__fadeInLeft"
                     >
                     </img>
                 </div>
                 <div className="col-8">
-                    <h3>{personajes[ids-1].name}</h3>
+                    <h3>{personaje.name}</h3>
                     <ul className="list-group list-group-flush">
-                        <li className="list-group-item"><b>status:</b> {personajes[ids-1].status}</li>
-                        <li className="list-group-item"><b>species:</b> {personajes[ids-1].species}</li>
-                        <li className="list-group-item"><b>type:</b> {personajes[ids-1].type}</li>
-                        <li className="list-group-item"><b>gender:</b> {personajes[ids-1].gender}</li>
-                        <li className="list-group-item"><b>origen:</b> {personajes[ids-1].origin.name}</li>
+                        <li className="list-group-item"><b>status:</b> {personaje.status}</li>
+                        <li className="list-group-item"><b>species:</b> {personaje.species}</li>
+                        <li className="list-group-item"><b>type:</b> {personajes.type}</li>
+                        <li className="list-group-item"><b>gender:</b> {personaje.gender}</li>
+                        <li className="list-group-item"><b>origen:</b> {personaje.origin.name}</li>
                     </ul>
                 </div>
             </div>
