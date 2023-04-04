d0fad4e736419a7cb111e03d8297661a32a0b736
nico1612 <nicogonzalezraggi123@hotmail.com>
2023-04-04 12:23:04 -0300

    w

--- src/ui/Navbar.jsx
+++ src/ui/Navbar.jsx
@@ -13,8 +13,7 @@ export const Navbar=()=>{
                 </Link>
 
                 <button className="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"> </span>
-                      
+                    <span className="navbar-toggler-icon"> </span>         
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
